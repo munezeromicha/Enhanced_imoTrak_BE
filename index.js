b10c4588900f3917038970052354a6bcb94f710f
Mr-Ndi <onlythenotes@gmail.com>
2025-05-21 12:09:53 +0200

    Complete login route

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import authRoutes from './src/auth/auth.routes.js';
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => res.send('Imotarak Backend API is live!'));
 
