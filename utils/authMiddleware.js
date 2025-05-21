ecb4baf7cdf88a69275c6d0e4fce14389afbdff6
Mr-Ndi <onlythenotes@gmail.com>
2025-05-21 12:11:47 +0200

    Authentication middle ware in Utils

---
+++ utils/authMiddleware.js
@@ -0,0 +1,15 @@
+import { verifyToken } from './jwt.js';
+
+export const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ error: 'No token provided' });
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(403).json({ error: 'Invalid token' });
+  }
+};
