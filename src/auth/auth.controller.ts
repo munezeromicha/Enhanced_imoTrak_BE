99ed9b6338af6d34f4f0d781b2b426ca3e3af0fb
Mr-Ndi <onlythenotes@gmail.com>
2025-05-23 19:11:12 +0200

    After working authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,7 @@ export const handleLogin = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
     const token = await login(req.body);
-    res.json({ token });
+    res.json( token );
   } catch (err: any) {
     res.status(401).json({ error: err.message });
   }
