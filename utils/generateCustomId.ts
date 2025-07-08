f8e701ff222fd87a224715fd5484050bf21d5631
Mr-Ndi <onlythenotes@gmail.com>
2025-07-08 18:53:20 +0200

    Repository structure

--- utils/generateCustomId.ts
+++
@@ -1,8 +0,0 @@
-export function generateCustomId(orgName: string): string {
-  const words = orgName
-    .split(/\s+/)
-    .map(w => w[0].toUpperCase())
-    .join('');
-  const year = new Date().getFullYear().toString().slice(-2);
-  return `${words}-${year}`;
-}
--- utils/hash.ts
+++
@@ -1,9 +0,0 @@
-import argon2 from 'argon2';
-
-export const hashPassword = async (plainPassword: any) => {
-  return await argon2.hash(plainPassword);
-};
-
-export const verifyPassword = async (hash: any, plainPassword: any) => {
-  return await argon2.verify(hash, plainPassword);
-};
